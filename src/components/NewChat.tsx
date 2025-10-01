c7301fcc479e33bf059acaa474144e0ddc3c92db
刘江伟 <ljw2693287507@163.com>
2025-10-01 16:14:24 +0800

    fix：新对话页面样式调整

--- src/components/NewChat.tsx
+++ src/components/NewChat.tsx
@@ -1,6 +1,7 @@
-import { Textarea, Box, Text, FileUpload, Image, HStack, Flex, useFileUpload } from "@chakra-ui/react";
+import { Textarea, Box, Text, FileUpload, Image, HStack, Flex, useFileUpload, chakra } from "@chakra-ui/react";
 import IconPaperclip from "@/assets/icon-paperclip.png";
 import IconGlobe from "@/assets/icon-globe.png";
+import IconGlobeSvg from "@/assets/icon-globe.svg";
 import IconArrowUp from "@/assets/icon-arrow-up.png";
 import { useState } from "react";
 
@@ -29,7 +30,7 @@ export function NewChat() {
         Chat AI
       </Text>
       <Box position="relative">
-        <Textarea minH="120px" p="20px" pb="60px" fontSize="16px" borderRadius="8px" resize="none" placeholder="开始对话..." color="#86807B" bgColor="#363131" borderColor="#5E565699" />
+        <Textarea minH="120px" p="20px" pb="60px" fontSize="16px" borderRadius="8px" resize="none" placeholder="开始对话..." _placeholder={{color: '#86807B'}} color="#fdfcfb" bgColor="#363131" borderColor="#5E565699" />
         <Box w="calc(100% - 35px)" h="60px" borderRadius="8px" position="absolute" bottom="7px" left="15px" zIndex="2" bgColor="#363131">
           <HStack pos="absolute" bottom="15px" left="5px">
             <FileUpload.RootProvider value={fileUpload}>
@@ -48,7 +49,10 @@ export function NewChat() {
             </FileUpload.RootProvider>
             <Flex minW="88px" h="28px" ml="20px" cursor="pointer" alignItems="center" backgroundColor="#4a4545" borderRadius="28px" align="center" justifyContent="center" gap="5px" onClick={onDeepThink}>
               <Box position="relative" w="16px" h="16px">
-                <Image src={IconGlobe} alt="" w="full" h="full" objectFit="contain" />
+                {isDeepThink ? <chakra.svg>
+                  <IconGlobeSvg/>
+                </chakra.svg>
+                : <Image src={IconGlobe} alt="" w="full" h="full" objectFit="contain" />}
               </Box>
               <Text fontSize="14px" fontFamily="PingFang SC" color={isDeepThink ? "#0660fc" : "#FDFCFB"}>
                 深度思考
@@ -56,7 +60,7 @@ export function NewChat() {
             </Flex>
           </HStack>
           <HStack pos="absolute" bottom="15px" right="5px">
-            <Flex w="28px" h="28px" borderRadius="28px" cursor="pointer" backgroundColor="#999696" justifyContent="center" alignItems="center" onClick={onSend}>
+            <Flex w="28px" h="28px" borderRadius="28px" cursor="pointer" backgroundColor="#ffdfdfff" justifyContent="center" alignItems="center" onClick={onSend}>
               <Image src={IconArrowUp} alt="" w="full" h="full" objectFit="contain" />
             </Flex>
           </HStack>
